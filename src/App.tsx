05a757928c9420c0637324b1ccebe7232165c70a
Raul Santana <ti.raul77@gmail.com>
2024-09-21 13:57:22 -0300

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Toaster richColors expand />
 
-      <Toaster richColors expand />
-
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
