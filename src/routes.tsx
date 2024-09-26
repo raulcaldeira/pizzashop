c4b552fbe9161d8440ef27ba8636a83420acf58d
Raul Santana <ti.raul77@gmail.com>
2024-09-25 21:18:10 -0300

    feat: create dashbord in home page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,8 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/app/dashboard'
+import { NotFound } from './pages/404'
+import { Dashboard } from './pages/app/dashboard/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 import { Orders } from './pages/orders/orders'
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
